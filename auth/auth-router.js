1b12c6697ca085524e3a688ed498fe82090c84d0
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-18 13:38:26 -0800

    migrations set up

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,4 +2,13 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const Users = require('../users/users-model.js');
+
+router.post('/register', (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  
+})
 module.exports = router;
