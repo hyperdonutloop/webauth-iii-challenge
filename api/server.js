fada90dc076561f0bb71add36f3db2b699f583b6
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-18 11:47:18 -0800

    dependencies added, basic file structure set up, server is up and running

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const authRouter = require('../auth/auth-router.js');
+const usersRouter = require('../users/users-router.js');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/auth', authRouter);
+server.use('api/users', usersRouter);
+
+module.exports = server;
