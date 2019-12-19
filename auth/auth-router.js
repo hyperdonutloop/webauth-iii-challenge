bca38a6076d1b864be9cb995aca9a3ac5f594a59
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-18 18:07:39 -0800

    fixed get request bug. all enpoints are working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -9,6 +9,47 @@ router.post('/register', (req, res) => {
   const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
 
-  
-})
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved)
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = signToken(user);
+
+        res.status(200).json({
+          token,
+          message: `Welcome ${user.username}!`
+        })
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials 💀' })
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+});
+
+function signToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const secret = process.env.JWT_SECRET || 'cookies are delicious';
+  const options = {
+    expiresIn: '1h',
+  }
+  return jwt.sign(payload, secret, options)
+}
+
 module.exports = router;
